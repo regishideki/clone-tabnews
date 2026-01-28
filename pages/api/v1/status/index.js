8599c1d6cc9711f95216b45e7f9ce40ff48d7901
Regis Hideki Hattori <regis_hattori@yahoo.com.br>
2026-01-28 07:27:17 -0300

    add not env to status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -6,14 +6,18 @@ const getStatus = async (req, res) => {
   const databaseVersionResult = await database.query("show server_version;");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
-  const databaseMaxConnectionsResult = await database.query("show max_connections;");
-  const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0].max_connections;
+  const databaseMaxConnectionsResult = await database.query(
+    "show max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
 
   const databaseOpenedConnectionsResult = await database.query({
     text: "select count(*)::int from pg_stat_activity where datname = $1;",
-    values: [process.env.POSTGRES_DB]
+    values: [process.env.POSTGRES_DB],
   });
-  const databaseOpenedConnectionsValue = databaseOpenedConnectionsResult.rows[0].count;
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
 
   res.status(200).json({
     updated_at: updatedAt,
@@ -22,9 +26,10 @@ const getStatus = async (req, res) => {
         version: databaseVersionValue,
         max_connections: parseInt(databaseMaxConnectionsValue),
         opened_connections: databaseOpenedConnectionsValue,
-      }
+        env: process.env.NODE_ENV,
+      },
     },
   });
-}
+};
 
-export default getStatus
+export default getStatus;
