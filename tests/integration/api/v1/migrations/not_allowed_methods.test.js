d527e6291e59ba7aefcce606d37a56753914dfcf
Regis Hideki Hattori <regis_hattori@yahoo.com.br>
2026-01-29 06:50:23 -0300

    improve migration endpoint to deal with leak connections

---
+++ tests/integration/api/v1/migrations/not_allowed_methods.test.js
@@ -0,0 +1,21 @@
+import { cleanDatabase } from "tests/utils.js";
+
+beforeAll(cleanDatabase);
+
+["DELETE", "HEAD", "PUT", "PATCH", "OPTIONS", "TRACE"].forEach((method) => {
+  test(`${method} /api/v1/migrations returns error and does not leak db connection`, async () => {
+    const migrationResponse = await fetch(
+      "http://localhost:3030/api/v1/migrations",
+      {
+        method,
+      },
+    );
+
+    expect(migrationResponse.status).toBe(405);
+
+    const statusResponse = await fetch("http://localhost:3030/api/v1/status");
+
+    const statusResponseBody = await statusResponse.json();
+    expect(statusResponseBody.dependencies.database.opened_connections).toBe(1);
+  });
+});
