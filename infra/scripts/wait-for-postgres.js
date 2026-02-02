d23baec56af3a125fb4b0a0a784a550114c61b6d
Regis Hideki Hattori <regis_hattori@yahoo.com.br>
2026-02-02 07:56:17 -0300

    prepare dev environment with only one command

---
+++ infra/scripts/wait-for-postgres.js
@@ -0,0 +1,17 @@
+const {exec } = require ("node:child_process");
+
+const checkPostgres = () => {
+  exec("docker exec postgres-dev pg_isready --host localhost", (error, stdout, stderr) => {
+    if (stdout.search("accepting connections") === -1) {
+      process.stdout.write(".");
+
+      checkPostgres();
+
+      return;
+    }
+    console.log("\n🟢 Postgres está aceitando conexões");
+  });
+};
+
+console.log("🔴 Aguardando Postgres aceitar conexões");
+checkPostgres();
