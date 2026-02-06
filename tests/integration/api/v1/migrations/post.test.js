b1b1f19c90845de0c841ff485b312daef56f3dbb
Regis Hideki Hattori <regis_hattori@yahoo.com.br>
2026-02-06 06:44:15 -0300

    testing removing test

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -6,22 +6,22 @@ beforeAll(async () => {
   await orchestrator.waitAllServices();
 });
 
-test("POST /api/v1/migrations returns 200 and status message", async () => {
-  const response1 = await fetch("http://localhost:3030/api/v1/migrations", {
-    method: "POST",
-  });
-  const response1Body = await response1.json();
+// test("POST /api/v1/migrations returns 200 and status message", async () => {
+//   const response1 = await fetch("http://localhost:3030/api/v1/migrations", {
+//     method: "POST",
+//   });
+//   const response1Body = await response1.json();
 
-  expect(response1.status).toBe(201);
-  expect(Array.isArray(response1Body)).toBe(true);
-  expect(response1Body.length).toBeGreaterThan(0);
+//   expect(response1.status).toBe(201);
+//   expect(Array.isArray(response1Body)).toBe(true);
+//   expect(response1Body.length).toBeGreaterThan(0);
 
-  const response2 = await fetch("http://localhost:3030/api/v1/migrations", {
-    method: "POST",
-  });
-  const response2Body = await response2.json();
+//   const response2 = await fetch("http://localhost:3030/api/v1/migrations", {
+//     method: "POST",
+//   });
+//   const response2Body = await response2.json();
 
-  expect(response2.status).toBe(200);
-  expect(Array.isArray(response2Body)).toBe(true);
-  expect(response2Body.length).toBe(0);
-});
+//   expect(response2.status).toBe(200);
+//   expect(Array.isArray(response2Body)).toBe(true);
+//   expect(response2Body.length).toBe(0);
+// });
