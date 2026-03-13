3ef2a16613640dac7d100f82a6c578ae79ae922c
Regis Hideki Hattori <regis_hattori@yahoo.com.br>
2026-03-13 08:58:19 -0300

    feat: returns json even when there is a 500 error

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,35 +1,42 @@
 import database from "infra/database.js";
+import { InternalServerError } from "infra/errors.js";
 
 const getStatus = async (req, res) => {
-  const updatedAt = new Date().toISOString();
+  try {
+    const updatedAt = new Date().toISOString();
 
-  const databaseVersionResult = await database.query("show server_version;");
-  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+    const databaseVersionResult = await database.query("show server_version;");
+    const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
-  const databaseMaxConnectionsResult = await database.query(
-    "show max_connections;",
-  );
-  const databaseMaxConnectionsValue =
-    databaseMaxConnectionsResult.rows[0].max_connections;
+    const databaseMaxConnectionsResult = await database.query(
+      "show max_connections;",
+    );
+    const databaseMaxConnectionsValue =
+      databaseMaxConnectionsResult.rows[0].max_connections;
 
-  const databaseOpenedConnectionsResult = await database.query({
-    text: "select count(*)::int from pg_stat_activity where datname = $1;",
-    values: [process.env.POSTGRES_DB],
-  });
-  const databaseOpenedConnectionsValue =
-    databaseOpenedConnectionsResult.rows[0].count;
+    const databaseOpenedConnectionsResult = await database.query({
+      text: "select count(*)::int from pg_stat_activity where datname = $1;",
+      values: [process.env.POSTGRES_DB],
+    });
+    const databaseOpenedConnectionsValue =
+      databaseOpenedConnectionsResult.rows[0].count;
 
-  res.status(200).json({
-    updated_at: updatedAt,
-    dependencies: {
-      database: {
-        version: databaseVersionValue,
-        max_connections: parseInt(databaseMaxConnectionsValue),
-        opened_connections: databaseOpenedConnectionsValue,
-        env: process.env.NODE_ENV,
+    res.status(200).json({
+      updated_at: updatedAt,
+      dependencies: {
+        database: {
+          version: databaseVersionValue,
+          max_connections: parseInt(databaseMaxConnectionsValue),
+          opened_connections: databaseOpenedConnectionsValue,
+          env: process.env.NODE_ENV,
+        },
       },
-    },
-  });
+    });
+  } catch (error) {
+    const publicError = new InternalServerError({ cause: error });
+    console.error(publicError);
+    res.status(500).json(publicError);
+  }
 };
 
 export default getStatus;
